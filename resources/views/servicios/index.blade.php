f7acfee2ffd8128b546d4130d963fa22d82cbb71
cynth12 <gonzalez.cynth12@gmail.com>
2025-09-16 18:19:45 -0600

    href

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -5,15 +5,15 @@
                 <h2 class="wow fadeInLeft" data-wow-dalay=".2s">Nuestros <span class="text-primary">Servicios</span> <br
                         class="d-lg-block d-none"></h2>
             </div>
-            
+
         </div>
         <div class="row row-30 justify-content-center">
             <div class="col-sm-8 col-md-6 col-lg-4 wow fadeInUp" data-wow-dalay=".1s">
                 <div class="icon-box-modern">
-                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/infeccion_herida.jpg') }}" alt=""
-                            width="200" height="100" />
+                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/infeccion_herida.jpg') }}"
+                            alt="" class="img-servicio" />
                     </div>
-                    <h4><a href="single-department.html">Infección de heridas</a></h4>
+                    <h4><a>Infección de heridas</a></h4>
                     <p>Tratamos heridas infectadas con protocolos avanzados de limpieza, desinfección y antibióticos
                         tópicos o sistémicos.
                         Nuestro enfoque busca controlar la infección, reducir la inflamación y acelerar la
@@ -22,10 +22,10 @@
             </div>
             <div class="col-sm-8 col-md-6 col-lg-4 wow fadeInUp" data-wow-dalay=".2s">
                 <div class="icon-box-modern">
-                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/quemadura.jpg') }}" alt=""
-                            width="250" height="200" />
+                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/quemadura.jpg') }}"
+                            alt="" class="img-servicio" />
                     </div>
-                    <h4><a href="single-department.html">Quemaduras</a></h4>
+                    <h4><a>Quemaduras</a></h4>
                     <p>Atendemos quemaduras leves y moderadas con terapias regenerativas, apósitos especiales y control
                         del dolor.
                         Nos enfocamos en evitar complicaciones y minimizar las secuelas estéticas.</p>
@@ -33,46 +33,79 @@
             </div>
             <div class="col-sm-8 col-md-6 col-lg-4 wow fadeInUp" data-wow-dalay=".3s">
                 <div class="icon-box-modern">
-                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/dehicensia_herida.jpg') }}" alt=""
-                            width="300" height="250" />
+                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/dehicensia_herida.jpg') }}"
+                            alt="" class="img-servicio" />
                     </div>
-                    <h4><a href="single-department.html">Dehiscencia de heridas</a></h4>
-                    <p>Cuando una herida quirúrgica se abre antes de cicatrizar, intervenimos con técnicas de cierre secundario, 
-                      control de humedad y seguimiento continuo para evitar infecciones y favorecer el cierre natural.
+                    <h4><a>Dehiscencia de heridas</a></h4>
+                    <p>Cuando una herida quirúrgica se abre antes de cicatrizar, intervenimos con técnicas de cierre
+                        secundario,
+                        control de humedad y seguimiento continuo para evitar infecciones y favorecer el cierre natural.
                     </p>
                 </div>
             </div>
             <div class="col-sm-8 col-md-6 col-lg-4 wow fadeInUp" data-wow-dalay=".4s">
                 <div class="icon-box-modern">
-                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/herida_quirurgica.jpg') }}"  alt=""
-                            width="250" height="200" />
+                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/herida_quirurgica.jpg') }}"
+                            alt="" class="img-servicio" />
                     </div>
-                    <h4><a href="single-department.html">Heridas quirurgicas</a></h4>
-                    <p>Realizamos seguimiento postoperatorio para asegurar que las incisiones quirúrgicas cicatricen correctamente. 
-                      Aplicamos cuidados específicos según el tipo de cirugía y el estado de la piel.</p>
+                    <h4><a>Heridas quirurgicas</a></h4>
+                    <p>Realizamos seguimiento postoperatorio para asegurar que las incisiones quirúrgicas cicatricen
+                        correctamente.
+                        Aplicamos cuidados específicos según el tipo de cirugía y el estado de la piel.</p>
                 </div>
             </div>
             <div class="col-sm-8 col-md-6 col-lg-4 wow fadeInUp" data-wow-dalay=".5s">
                 <div class="icon-box-modern">
-                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/ulcera.jpg') }}" alt=""
-                            width="200" height="200" />
+                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/ulcera.jpg') }}"
+                            alt="" class="img-servicio" />
                     </div>
-                    <h4><a href="single-department.html">Ulceras por presion</a></h4>
-                    <p>Tratamos lesiones por presión en pacientes encamados con tecnología especializada, 
-                      cambios posturales y apósitos que favorecen la regeneración del tejido.</p>
+                    <h4><a>Ulceras por presion</a></h4>
+                    <p>Tratamos lesiones por presión en pacientes encamados con tecnología especializada,
+                        cambios posturales y apósitos que favorecen la regeneración del tejido.</p>
                 </div>
             </div>
             <div class="col-sm-8 col-md-6 col-lg-4 wow fadeInUp" data-wow-dalay=".6s">
                 <div class="icon-box-modern">
-                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/estoma.jpg') }}" alt=""
-                            width="200" height="150" />
+                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/estoma.jpg') }}"
+                            alt="" class="img-servicio" />
                     </div>
-                    <h4><a href="single-department.html">Estomas</a></h4>
-                    <p>Brindamos cuidados integrales para estomas, asegurando que el área se mantenga limpia,  protegida y funcional. 
-                      Educamos al paciente en su manejo diario para evitar complicaciones.</p> 
-                       
+                    <h4><a>Estomas</a></h4>
+                    <p>Brindamos cuidados integrales para estomas, asegurando que el área se mantenga limpia, protegida
+                        y funcional.
+                        Educamos al paciente en su manejo diario para evitar complicaciones.</p>
+
                 </div>
             </div>
+            <div class="col-sm-8 col-md-6 col-lg-4 wow fadeInUp" data-wow-dalay=".6s">
+                <div class="icon-box-modern">
+                    <div class="icon-box-modern__media"><img src="{{ asset('assets/images/pie-diabetico.png') }}"
+                            alt="" class="img-servicio" />
+                    </div>
+                    <h4><a>Pie diabetico</a></h4>
+                    <p>El pie diabético es una complicación crónica de la diabetes que afecta los nervios y
+                        vasos sanguíneos de los pies. Esta condición reduce la sensibilidad,
+                        o que hace que pequeñas heridas pasen desapercibidas y se infecten con facilidad.
+                        Las úlceras, infecciones profundas e incluso la gangrena
+                        pueden desarrollarse si no se trata a tiempo, llegando en casos graves a requerir amputaciones.
+                    </p>
+
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-6 col-lg-4 d-none d-lg-block">
+                <div class="icon-box-modern" style="visibility: hidden;">
+                    <div class="icon-box-modern__media"></div>
+                    <h4><a>&nbsp;</a></h4>
+                    <p>&nbsp;</p>
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-6 col-lg-4 d-none d-lg-block">
+                <div class="icon-box-modern" style="visibility: hidden;">
+                    <div class="icon-box-modern__media"></div>
+                    <h4><a>&nbsp;</a></h4>
+                    <p>&nbsp;</p>
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
