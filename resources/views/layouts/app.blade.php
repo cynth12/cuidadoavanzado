82203b4ffbf1baa7b38805142f947b2a52aebf64
cynth12 <gonzalez.cynth12@gmail.com>
2025-10-28 23:33:22 -0700

    mal escrito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     {{-- Open Graph --}}
     <meta property="og:title" content="@yield('og_title', 'CUIDADO AVANZADO 🦶 Clínica Especializada en Heridas y Estomas')">
     <meta property="og:description" content="@yield('og_description', 'Expertos en curación de heridas, ostomías y pie diabético en Tulancingo. Agenda tu cita hoy.')">
-    <meta property="og:image" content="https://cuidadovanzado.com/assets/images/academia.jpg">
+    <meta property="og:image" content="https://cuidadoavanzado.com/assets/images/academia.png">
     <meta property="og:url" content="https://cuidadoavanzado.com/">
     <meta property="og:type" content="website">
     <meta property="og:image:width" content="1953" />
